ad3c084c5dba96b9b5da1f1ad387cedd71f886bd
zachsea <zachse4@gmail.com>
2026-02-28 12:35:13 -0800

    cicd

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,17 @@ allprojects {
         mavenCentral()
     }
 }
+
+tasks.register("build") {
+    dependsOn(
+        gradle.includedBuild("lib").task(":build"),
+        gradle.includedBuild("gtnh-1.7.10").task(":build")
+    )
+}
+
+tasks.register("clean") {
+    dependsOn(
+        gradle.includedBuild("lib").task(":clean"),
+        gradle.includedBuild("gtnh-1.7.10").task(":clean")
+    )
+}
--- gtnh-1.7.10/build.gradle.kts
+++ gtnh-1.7.10/build.gradle.kts
@@ -1,8 +1,14 @@
-
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
 
 dependencies {
     implementation("cafe.zach:greg-link-lib:0.0.1")
+}
+
+val modVersion = System.getenv("GITHUB_REF_NAME")?.removePrefix("v") ?: "dev"
+
+tasks.withType<Jar> {
+    archiveBaseName.set("greg-link-gtnh-1.7.10")
+    archiveVersion.set(modVersion)
 }
